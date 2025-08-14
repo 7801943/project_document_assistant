20ea1747abc8165ec835e4825f563c39bc1d9601
7801943 <130665760+7801943@users.noreply.github.com>
2025-08-14 14:02:45 +0800

    Delete claude_code.sh

--- claude_code.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 设置环境变量
-export ANTHROPIC_BASE_URL="https://api.moonshot.cn/anthropic/"
-export ANTHROPIC_AUTH_TOKEN="sk-7GUfyabVxWR9iTurMSCfd3Ln3aFF8DMgmOj8M2N40XLlXvEL"
-
-# 启动 claude-code
-claude
